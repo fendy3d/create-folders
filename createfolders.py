500aa8ed1260cef2642e1c8f64f37c7be0bef2ab
Fendy Lieanata <fendylieanata@Fendys-MacBook-Pro.local>
2022-04-18 21:08:52 +0700

    first commit

---
+++ createfolders.py
@@ -0,0 +1,19 @@
+import os
+
+folderNames = [
+"pg 3 - ow pronunciation",
+"pg 4 - ow reading",
+"pg 7 - ou pronunciation",
+"pg 8 - ou reading",
+"pg 15 - ar pronunciation",
+"pg 16 - ar reading",
+"pg 21 - ae pronunciation",
+"pg 22 - ae reading",
+"pg 25 - ie pronunciation",
+"pg 26 - ie reading",
+"pg 33 - oe ue pronunciation",
+"pg 34 - oe ue reading",
+]
+
+for name in folderNames:
+	os.mkdir(name)
